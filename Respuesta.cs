ea66b6b83689b84a054f452131f3347ad6c203e0
Gen <41450130+Gen13673@users.noreply.github.com>
2020-09-03 21:55:40 -0300

    clase respuesta

---
+++ Respuesta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estacionamientos
+{
+    class Respuesta
+    {
+        public bool hayCambio;
+        public int importe;
+    }
+}
