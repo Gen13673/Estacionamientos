25abbc2519bc03ec20e9ac9c2d7cb5979f053b74
Gen <41450130+Gen13673@users.noreply.github.com>
2020-09-03 21:56:31 -0300

    Add files via upload

---
+++ Cajero.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estacionamientos
+{
+    class Cajero
+    {
+        public int importe;
+        public int cambio;
+        public Respuesta respuesta;
+
+        public Respuesta calcularRespuesta(string ticket)
+        {
+            importe = Int32.Parse(ticket);
+            respuesta.importe = importe;
+            if (cambio>0)
+            {
+                respuesta.hayCambio = true;
+            }
+            else
+            {
+                respuesta.hayCambio = false;
+            }
+            return respuesta;
+        }
+        public int devolverCambio(int dinero)
+        {
+            int cambioADevolver = 0;
+
+            if ((dinero - respuesta.importe) > 0)
+            {
+                cambioADevolver = dinero - respuesta.importe;
+            }
+            return cambioADevolver;
+        }
+        public string devolverTicket()
+        {
+            string ticket = importe.ToString();
+            return ticket;
+        }
+        public void sonarPitido()
+        {
+
+        }
+        public void tragarTicket()
+        {
+
+        }
+
+    }
+}
